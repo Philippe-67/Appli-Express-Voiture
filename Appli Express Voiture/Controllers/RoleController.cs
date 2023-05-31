028546a6a1f265b704ab9d95591d5254fcf3cb90
Philippe CHARLES <philippe.charles@axa.fr>
2023-05-31 23:36:09 +0200

    Ajoutez des fichiers projet.

---
+++ Appli Express Voiture/Controllers/RoleController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Security.Claims;
+
+namespace Appli_Express_Voiture.Controllers
+{
+    [Authorize]
+    public class RoleController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<IdentityUser> _userManager;
+       // private readonly object await_userManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            // _userManager = userManager;
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View(new IdentityRole());
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(IdentityRole role)
+        {
+            await _roleManager.CreateAsync(role);
+            return View();
+        }
+        public async Task<IActionResult> GetMyRoles()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            var user = await _userManager.FindByEmailAsync(userEmail);
+            var roles = await _userManager.GetRolesAsync(user);
+            return View(roles);
+        }
+    }
+}
+
---
+++ Appli Express Voiture/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Appli_Express_Voiture.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
