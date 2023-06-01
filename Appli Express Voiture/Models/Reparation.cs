958f8e8b3d38557d19b5ba038900287fe549efc9
Philippe CHARLES <philippe.charles@axa.fr>
2023-06-01 12:15:58 +0200

    creation model voiture et model repartion

--- Appli Express Voiture/Models/Reparation.cs
+++ Appli Express Voiture/Models/Reparation.cs
@@ -1,6 +1,12 @@
 ﻿namespace Appli_Express_Voiture.Models
 {
     public class Reparation
+
     {
+        public int Id { get; set; }
+        public string TypeIntervention { get; set; }
+        public string  CoutIntervention{ get; set;}
+        public int VoitureId { get; set; }
+        public Voiture? Voiture { get; set; }
     }
 }
--- Appli Express Voiture/Models/Voiture.cs
+++ Appli Express Voiture/Models/Voiture.cs
@@ -2,5 +2,10 @@
 {
     public class Voiture
     {
+        public int Id { get; set; }
+        public string Marque{ get; set; }
+        public string Modele { get; set; }
+
+        public ICollection<Reparation> Reparation { get; set; }
     }
 }
